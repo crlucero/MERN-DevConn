2f2466ecc986033e82d5b2ba89065842256b78c9
Cristian Lucero <cristianjlucero32@gmail.com>
2019-05-10 15:40:19 -0700

    Register form is now updated with state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+// import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  return <Fragment>Login</Fragment>;
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,98 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
-  return <div>Register</div>;
+  // useState Hook
+  // Equal to...
+  // state = {
+  //     formDate: {
+  //         name: ''...
+  //     }
+  // }
+  //   setFormData equal to this.setState({name: name})
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  // Make a copy of form data with spread oper. and change name to value of input
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user" />
+        Create Your Account
+      </p>
+      <form onSubmit={e => onSubmit(e)} className="form">
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar, so if you want an avatar, use a Gravatar
+            email.
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            minLength="6"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            minLength="6"
+            name="password2"
+            value={password2}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" value="Register" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Register;
