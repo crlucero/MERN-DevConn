9455924fd54e066ef1dc0d1acf5c964b588b0985
Cristian Lucero <cristianjlucero32@gmail.com>
2019-05-20 14:40:26 -0700

    Implemented profiletop/profileabout to profile page

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addExperience } from '../../actions/profileAction';
-// import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 const AddExperience = ({ addExperience, history }) => {
   const [formData, setFormData] = useState({
@@ -126,4 +126,4 @@ AddExperience.propTypes = {
 export default connect(
   null,
   { addExperience }
-)(AddExperience);
+)(withRouter(AddExperience));
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -8,7 +8,7 @@ import { getProfiles } from '../../actions/profileAction';
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
     getProfiles();
-  }, []);
+  }, [getProfiles]);
 
   return (
     <Fragment>
