aa95c4cb81785960d557dd35f296e7c69236a557
Cristian Lucero <cristianjlucero32@gmail.com>
2019-05-20 12:50:14 -0700

    List of all profiles now displayed

--- client/src/actions/profileAction.js
+++ client/src/actions/profileAction.js
@@ -3,6 +3,8 @@ import { setAlert } from './alertAction';
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
@@ -26,6 +28,24 @@ export const getCurrentProfile = () => async dispatch => {
   }
 };
 
+// Get All Profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+  try {
+    const res = await axios.get('/api/profile');
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
 // Get profile by ID
 export const getProfileById = userId => async dispatch => {
   try {
@@ -33,6 +53,22 @@ export const getProfileById = userId => async dispatch => {
 
     dispatch({
       type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+// Get github repos
+export const getGithubRepos = username => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+
+    dispatch({
+      type: GET_REPOS,
       payload: res.data
     });
   } catch (err) {
